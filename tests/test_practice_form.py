697151dbba5a0460135f966841bdc0010e072cca
Chesnova <myjobistester@gmail.com>
2022-09-18 23:03:45 +0300

    add new tests

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,13 +1,16 @@
 import pytest
+import os
 from selene.support.shared import browser
-from selene import have, be
+from selene import have
 
 @pytest.fixture
 def open_browser():
     browser.config.timeout = 3
     browser.config.base_url = ('https://demoqa.com')
 
+
 def test_student_registration_form(open_browser):
+    # adding text in the registration form
     browser.open('/automation-practice-form')
     browser.element('#firstName').set_value('Salvador')
     browser.element('#lastName').type('Dali')
@@ -18,7 +21,49 @@ def test_student_registration_form(open_browser):
     browser.element('.react-datepicker__month-select').send_keys('April')
     browser.element('.react-datepicker__year-select').send_keys('2000')
     browser.element('.react-datepicker__day.react-datepicker__day--011').click()
-    # browser.with_(timeout=3).element('#dateOfBirthInput').should(have.value('11 Apr 2000'))
     browser.element('#subjectsInput').type('Computer Science').press_enter().type('Biology').press_enter()
     browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').send_keys()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('../resources/fortest.png'))
+    browser.element('#currentAddress').set_value('Figueres, Catalonia, Spain')
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
+    browser.element('#submit').press_enter()
+
+    # assert modal form
+    browser.element('.table-responsive').all('tbody tr').should(have.texts(
+        'Student Name Salvador Dali',
+        'Student Email test@test.test',
+        'Gender Male', 'Mobile 9999999999',
+        'Date of Birth 11 April,2000',
+        'Subjects Computer Science, Biology',
+        'Hobbies Music',
+        'Picture fortest.png',
+        'Address Figueres, Catalonia, Spain',
+        'State and City Haryana Karnal'
+    ))
+
+
+def test_web_tables_(open_browser):
+    # 1) add a new fourth line
+    browser.open('/webtables')
+    browser.element('#addNewRecordButton').click()
+    browser.element('#firstName').type('Salvador')
+    browser.element('#lastName').type('Dali')
+    browser.element('#userEmail').set_value('test@test.test')
+    browser.element('#age').type('31')
+    browser.element('#salary').type('5000')
+    browser.element('#department').type('Insurance')
+    browser.element('#submit').submit()
+
+    # 2) edit all fields in the second line
+    browser.element('#edit-record-2').click()
+    browser.element('#firstName').type('Salvadorito')
+    browser.element('#lastName').type('Dalila')
+    browser.element('#userEmail').set_value('test3@test.test')
+    browser.element('#age').type('31')
+    browser.element('#salary').type('5000')
+    browser.element('#department').type('Insurance')
+    browser.element('#submit').submit()
+
+    # 3) remove the third line
+    browser.element('#delete-record-3').click()
