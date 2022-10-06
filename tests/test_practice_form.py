6aac7563daa59acc3db70394f8cbf14c4857b7e8
Chesnova <myjobistester@gmail.com>
2022-10-06 21:47:07 +0300

    add video recorder

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -67,5 +67,6 @@ def test_student_registration_form():
         )
 
     attach.add_html(browser)
-    attach.add_logs(browser)
     attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
